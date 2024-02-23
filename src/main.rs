bba1e831e070808bbdece41550043a1cf49babce
Andrey Paderin <andy.paderin@gmail.com>
2024-02-24 02:41:23 +0300

    routing

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,235 @@ use sqlx::PgPool;
 use tokio::time::{self, Duration};
 use validator::Validate;
 
+enum ApiError {
+    SQL(sqlx::Error)
+}
+
+impl From<sqlx::Error> for ApiError {
+    fn from(e: sqlx::Error) -> Self {
+        Self::SQLError(e)
+    }
+}
+
+impl AxumIntoResponse for ApiError {
+    fn into_response(self) -> Response {
+        match self {
+            Self::SQLError(e) => {
+                (
+                    StatusCode::INTERNAL_SERVER_ERROR,
+                    format!("SQL Error: {e}")
+                ).into_response()
+            }
+        }
+    }
+}
+
+async fn create_website(
+    State(state): State<AppState>,
+    Form(new_website): Form<Website>,
+) -> Result<impl AxumIntoResponse, impl AxumIntoResponse> {
+    if new_website.validate().is_err() {
+        return Err((
+            StatusCode::INTERNAL_SERVER_ERROR,
+            "Validation error: is your website a reachable URL?",
+        ));
+    }
+
+    sqlx::query("INSERT INTO websites (url, alias) VALUES ($1, $2)")
+        .bind(new_website.url)
+        .bind(new_website.alias)
+        .execute(&state.db)
+        .await
+        .unwrap();
+
+    Ok(Redirect::to("/"))
+}
+
+#[derive(Serialize, Validate)]
+struct WebsiteInfo {
+    #[validate(url)]
+    url: String,
+    alias: String,
+    data: Vec<WebsiteStats>,
+}
+
+#[derive(sqlx::FromRow, Serialize)]
+pub struct WebsiteStats {
+    time: DateTime<Utc>,
+    uptime_pct: Option<i16>,
+}
+
+async fn get_websites(State(state): State<AppState>) -> Result<impl AskamaIntoResponse, ApiError> {
+    let websites = sqlx::query_as::<_, Website>("SELECT url, alias FROM websites")
+        .fetch_all(&state.db)
+        .await?;
+
+    let mut logs = Vec::new();
+
+    for website in websites {
+        let data = get_daily_stats(&website.alias, &state.db).await?;
+
+        logs.push(WebsiteInfo {
+            url: website.url,
+            alias: website.alias,
+            data,
+        });
+    }
+
+    Ok(WebsiteLogs { logs })
+}
+
+#[derive(sqlx::FromRow, Serialize)]
+pub struct WebsiteStats {
+    time: DateTime<Utc>,
+    uptime_pct: Option<i16>,
+}
+
+async fn get_daily_stats(alias: &str, db: &PgPool) -> Result<Vec<WebsiteStats>, ApiError> {
+    let data = sqlx
+    ::query_as::<_, WebsiteStats>(
+        r#"
+            SELECT date_trunc('hour', created_at) as time,
+            CAST(COUNT(case when status = 200 then 1 end) * 100 / COUNT(*) AS int2) as uptime_pct
+            FROM logs
+            LEFT JOIN websites on websites.id = logs.website_id
+            WHERE websites.alias = $1
+            group by time
+            order by time asc
+            limit 24
+            "#
+    )
+        .bind(alias)
+        .fetch_all(db).await?;
+
+    let number_of_splits = 24;
+    let number_of_seconds = 3600;
+
+    let data = fill_data_gaps(data, number_of_splits, SplitBy::Hour, number_of_seconds);
+
+    Ok(data)
+}
+
+enum SplitBy {
+    Hour,
+    Day
+}
+
+fn fill_data_gaps(
+    mut data: Vec<WebsiteStats>,
+    splits: i32,
+    format: SplitBy,
+    number_of_seconds: i32
+) -> Vec<WebsiteStats> {
+    // if the length of data is not as long as the number of required splits (24)
+    // then we fill in the gaps
+    if (data.len() as i32) < splits {
+        // for each split, format the time and check if the timestamp exists
+        for i in 1..24 {
+            let time = Utc::now() - chrono::Duration::seconds((number_of_seconds * i).into());
+            let time = time
+                .with_minute(0)
+                .unwrap()
+                .with_second(0)
+                .unwrap()
+                .with_nanosecond(0)
+                .unwrap();
+
+            let time = if matches!(format, SplitBy::Day) {
+                time.with_hour(0).unwrap()
+            } else {
+                time
+            };
+            // if timestamp doesn't exist, push a timestamp with None
+            if !data.iter().any(|x| x.time == time) {
+                data.push(WebsiteStats {
+                    time,
+                    uptime_pct: None,
+                });
+            }
+        }
+        // finally, sort the data
+        data.sort_by(|a, b| b.time.cmp(&a.time));
+    }
+
+    data
+}
+
+#[derive(Serialize, sqlx::FromRow, Template)]
+#[template(path = "single_website.html")]
+struct SingleWebsiteLogs {
+    log: WebsiteInfo,
+    incidents: Vec<Incident>,
+    monthly_data: Vec<WebsiteStats>,
+}
+
+#[derive(sqlx::FromRow, Serialize)]
+pub struct Incident {
+    time: DateTime<Utc>,
+    status: i16,
+}
+
+async fn get_website_by_alias(
+    State(state): State<AppState>,
+    Path(alias): Path<String>,
+) -> Result<impl AskamaIntoResponse, ApiError> {
+    let website = sqlx::query_as::<_, Website>("SELECT url, alias FROM websites WHERE alias = $1")
+        .bind(&alias)
+        .fetch_one(&state.db)
+        .await?;
+
+    let last_24_hours_data = get_daily_stats(&website.alias, &state.db).await?;
+    let monthly_data = get_monthly_stats(&website.alias, &state.db).await?;
+
+    let incidents = sqlx::query_as::<_, Incident>(
+        "SELECT logs.created_at as time,
+         logs.status from logs
+         left join websites on websites.id = logs.website_id
+         where websites.alias = $1 and logs.status != 200",
+    )
+        .bind(&alias)
+        .fetch_all(&state.db)
+        .await?;
+
+    let log = WebsiteInfo {
+        url: website.url,
+        alias,
+        data: last_24_hours_data,
+    };
+
+    Ok(SingleWebsiteLogs {
+        log,
+        incidents,
+        monthly_data,
+    })
+}
+
+async fn delete_website(
+    State(state): State<AppState>,
+    Path(alias): Path<String>,
+) -> Result<impl AxumIntoResponse, ApiError> {
+    let mut tx = state.db.begin().await?;
+    if let Err(e) = sqlx::query("DELETE FROM logs WHERE website_alias = $1")
+        .bind(&alias)
+        .execute(&mut *tx)
+        .await {
+        tx.rollback().await?;
+        return Err(ApiError::SQLError(e));
+    };
+
+    if let Err(e) = sqlx::query("DELETE FROM websites WHERE alias = $1")
+        .bind(&alias)
+        .execute(&mut *tx)
+        .await {
+        tx.rollback().await?;
+        return Err(ApiError::SQLError(e));
+    }
+
+    tx.commit().await?;
+
+    Ok(StatusCode::OK)
+}
+
 #[derive(Deserialize, sqlx::FromRow, Validate)]
 struct Website {
     #[validate(url)]
